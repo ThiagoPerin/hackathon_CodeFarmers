2fe4b3968ea64453cef62ad56fc2c8f55dccd0bb
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-29 01:22:44 -0300

    feat: new homeBtn button variant

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        homeBtn: "rounded-4xl bg-green-light text-green-mid hover:bg-green-mid hover:text-gray-light",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
