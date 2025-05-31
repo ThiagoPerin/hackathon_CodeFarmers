3677da5dbcf36774d6f00250a771ea7c994d133b
Thiago Perin <thiagoperin2310@gmail.com>
2025-05-31 20:26:19 -0300

    feat: add new onClick prop to round image button component and improves style

--- frontend/src/components/custom/buttons/RoundImageButton.tsx
+++ frontend/src/components/custom/buttons/RoundImageButton.tsx
@@ -3,12 +3,13 @@ import { ReactNode } from "react";
 type RoundImageButton = {
     children: ReactNode;
     label: string;
+    onClick?: () => void;
 };
 
-export default function RoundImageButton({ children, label }: RoundImageButton) {
+export default function RoundImageButton({ children, label, onClick }: RoundImageButton) {
     return (
-        <div className="w-[130px] flex flex-col items-center cursor-pointer gap-1">
-            <div className="w-full aspect-square relative flex items-center justify-center bg-green-light rounded-full overflow-hidden">
+        <div className="w-[130px] flex flex-col items-center cursor-pointer gap-1" onClick={onClick}>
+            <div className="w-full aspect-square relative flex items-center justify-center bg-green-light text-green-dark hover:bg-green-dark hover:text-gray-light rounded-full overflow-hidden">
                 {children}
             </div>
             <div className="text-md text-center">{label}</div>
