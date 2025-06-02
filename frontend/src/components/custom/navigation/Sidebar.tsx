5fce5981753ef07a3140adb81f4675166464e6fb
Thiago Perin <thiagoperin2310@gmail.com>
2025-06-01 23:09:38 -0300

    feat: add new components and redirects to sidebar

--- frontend/src/components/custom/navigation/Sidebar.tsx
+++ frontend/src/components/custom/navigation/Sidebar.tsx
@@ -1,13 +1,24 @@
+"use client"
 import Image from "next/image";
 import { House, Plus, Search } from 'lucide-react';
+import { useRouter } from "next/navigation";
+import SidebarButton from "../buttons/SidebarButton";
 
 export default function Sidebar() {
+    const router = useRouter()
+
     return (
         <div className="w-[60px] h-full bg-gray-300 flex flex-col items-center justify-start gap-4 py-4 px-2">
             <Image src="/logo-transparente.png" alt="Logo" width={60} height={60}></Image>
-            <House size={20} className="text-gray-700"/>
-            <Plus size={20} className="text-gray-700"/>
-            <Search size={20} className="text-gray-700"/>
+            <SidebarButton onClick={() => router.push("/dashboard")}>
+                <House size={20} />
+            </SidebarButton>
+            <SidebarButton onClick={() => router.push("/dashboard/upload-file")}>
+                <Plus size={20} />
+            </SidebarButton>
+            <SidebarButton onClick={() => router.push("/dashboard/projects")}>
+                <Search size={20} />
+            </SidebarButton>
         </div>
     );
 }
